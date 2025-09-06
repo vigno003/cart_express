407f6f424348257b695d8e3e36ba4ed886bdf10f
Luca Vignali <lucavigno2003@gmail.com>
2025-09-06 16:14:18 +0200

    small bug fix

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -43,7 +43,10 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
+      appBar: AppBar(
+        title: const Text('Login'),
+        automaticallyImplyLeading: false,
+      ),
       body: Consumer<LoginViewModel>(
         builder: (context, vm, _) {
           if (vm.loading) {
